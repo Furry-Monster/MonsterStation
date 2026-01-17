1d3da7ff76508130b85ddb984045586e9e909bda
furrymonster <4urrym0nster@gmail.com>
2026-01-17 19:46:17 +0800

    update config

--- src/config.ts
+++ src/config.ts
@@ -59,7 +59,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-  avatar: "assets/images/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: "Furry Monster",
   bio: "Code as an Artist, Live as a Dreamer.",
   links: [
